a3c7773059f7d864b2ff7a6b90f0d27c76edbd27
Soham Ghosh <156573535+orophile07@users.noreply.github.com>
2025-08-02 21:55:12 +0530

    Update qa_chain.py

--- qa_chain.py
+++ qa_chain.py
@@ -4,6 +4,6 @@ import streamlit as st
 
 def get_direct_llm():
     return ChatGroq(
-        api_key=st.secrets("groq_api_key"),
+        api_key=st.secrets["groq_api_key"],
         model_name="llama3-70b-8192"
     )
